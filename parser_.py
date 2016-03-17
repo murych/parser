dffc2ffac7b17623711bd67d83704eb7a1103f74
murych <murychkun@yahoo.com>
2016-03-17 23:57:46 +0300

    [WIP] re-written parser_.py in oop-style

--- parser_.py
+++ parser_.py
@@ -1,28 +1,54 @@
+import re
 import codecs
 import requests
 from bs4 import BeautifulSoup
-from params_ import urls, params
 
 
-def get_top(url):
-    container_tag, container_attrs, title_tag, title_attrs,\
-        author_tag, author_attrs = params(url)
-    r = requests.get(url)
-    r.encoding = 'utf-8'
-    soup = BeautifulSoup(r.text, 'html.parser')
-    container = soup.find(container_tag, attrs=container_attrs)
-    return list(map(lambda div: div.text, container.find_all(title_tag, title_attrs))),\
-        list(map(lambda div: div.text, container.find_all(author_tag, author_attrs)))
+def write_file(name, url, top_title):
+        with codecs.open('%s.txt' % name, 'w', encoding='utf-8') as f:
+            f.write(u'%s\n%s\n\n' % (name, url))
+            for title in top_title:
+                f.write('%s\n' % (title.strip('\n')))
 
 
-def parse(name):
-    top_title, top_author = get_top(urls[name])
-    with codecs.open('%s.txt' % name, 'w', encoding='utf-8') as f:
-        f.write(u'%s\n%s\n\n' % (name, urls[name]))
-        for title, author in zip(top_title, top_author):
-            f.write(u'%s - %s\n' % (' '.join(author.strip('\n').split()),
-                                    title.strip('\n')))
+class Site():
+    def __init__(self, url):
+        self.url = url
+        self.name = re.match('^(?:http?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)', url).group(1)
 
-if __name__ == '__main__':
-    for name in urls.keys():
-        parse(name)
+    def parse(self):
+        r = requests.get(self.url)
+        r.encoding = 'utf-8'
+        return BeautifulSoup(r.text, 'html.parser')
+
+    def get_top(self):
+        soup = self.parse()
+        container = soup.find(self.container_tag, attrs=self.container_attrs)
+        top_title = container.find_all(self.title_tag, self.title_attrs)
+        top_title = list(map(lambda title: title.text, top_title))
+        return self.name, self.url, top_title
+
+
+class LiveLib(Site):
+    def __init__(self, url):
+        Site.__init__(self, url)
+        self.title_tag, self.title_attrs = 'a', {'class': 'tag-book-title'}
+        self.container_tag, self.container_attrs = 'table', {'class': 'linear-list'}
+
+
+class Readly(Site):
+    def __init__(self, url):
+        Site.__init__(self.url)
+        self.title_tag, self.title_attrs = 'h3', {'class': 'blvi__title'}
+        self.container_tag, self.container_attrs = 'table', {'class': 'book-list-view'}
+
+
+class ReadRate(Site):
+    def __init__(self, url):
+        Site.__init__(self, url)
+        self.title_tag, self.title_attrs = 'div', {'class': 'title'}
+        self.container_tag, self.container_attrs = 'div', {'class': 'books-list'}
+
+livelib = LiveLib('http://www.livelib.ru/books/top')
+#readly = Readly('http://readly.ru/books/top/')
+readrate = ReadRate('http://readrate.com/rus/ratings/top100')
